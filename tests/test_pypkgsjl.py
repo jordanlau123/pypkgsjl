ae315da6e818acbe03e7c518368d1c4652eda157
jordanlau123 <jordannlau@gmail.com>
2021-02-24 22:05:02 -0800

    added unit tests for catbind

--- tests/test_pypkgsjl.py
+++ tests/test_pypkgsjl.py
@@ -1,5 +1,13 @@
 from pypkgsjl import __version__
 from pypkgsjl import pypkgsjl
+import pandas as pd
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((pypkgsjl.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((pypkgsjl.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
